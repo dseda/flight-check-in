14a2b61802efb836d2bccb4aff4b0f022c244a1e
sed <sedademirgf@gmail.com>
2021-07-27 10:25:00 +0100

    add row number and show selected seat number

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let rows = 25;
-let seatIds = ["A", "B", "C", "D", "E", "F"];
-let corridors = 1;
+let seatLetters = ["A", "B", "C", "D", "E", "F"];
 let columns = 3;
-let seatTotal = rows * (corridors + 1) * columns;
+let seatTotal = rows * 2 * columns;
+let info = document.getElementById("selection");
 addSeat();
 function addSeat() {
   const fuselage = document.getElementById("fuselage");
-  for (let s = 0; s < seatTotal; s++) {
-    let seat = document.createElement("div");
-    seat.classList.add("seat");
-    fuselage.appendChild(seat);
+  for (let r = 0; r < rows; r++) {
+    let corridor = document.createElement("div");
+    corridor.classList.add("corridor");
+    corridor.innerHTML = r + 1;
+    for (let i = 0; i < seatLetters.length; i++) {
+      let seat = document.createElement("div");
+      seat.classList.add("seat");
+      seat.id = "seat";
+      seat.innerHTML = seatLetters[i];
+      if (i === 3) {
+        fuselage.appendChild(corridor);
+        fuselage.appendChild(seat);
+      } else {
+        fuselage.appendChild(seat);
+      }
+    }
   }
 }
 const seats = document.getElementsByClassName("seat");
@@ -20,18 +32,20 @@ for (let i = 0; i < seats.length; i++) {
   });
 }
 for (let i = 0; i < seats.length; i++) {
-  seats[i].addEventListener("mouseup", function () {
-    seats[i].classList.remove("selected");
+  seats[i].addEventListener("mouseup", function (e) {
+    e.target.classList.add("reserved");
+    seat = document.getElementById("seat");
+    info.innerHTML = e.target.innerHTML;
   });
 }
-for (let i = 0; i < seats.length; i++) {
-  seats[i].addEventListener("dblclick", function () {
-    seats[i].classList.add("reserved");
-  });
-}
+// for (let i = 0; i < seats.length; i++) {
+//   seats[i].addEventListener("dblclick", function () {
+//     seats[i].classList.add("reserved");
+//   });
+// }
 
 // const seat_ = {
-//   number: "A",
+//   column: "A",
 //   row: 1,
 //   reserved: false,
 //   selected: false,
